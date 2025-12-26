ba36b17406b3ef5228caf3f14cd552f07a66c421
orrans <orrans@gmail.com>
2025-12-27 00:08:03 +0200

    Add wishlist logic

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -7,11 +7,17 @@ import { Heart } from 'lucide-react'
 import { HeartIcon } from './icons/HeartIcon'
 import { Carousel } from './Carousel'
 import { formatPrice, getRandomIntInclusive } from '../services/util.service'
-
+import { useSelector } from 'react-redux'
+import { addToWishlist, removeFromWishlist } from '../store/actions/user.actions'
 
 export function StayPreview({ stay, fromDate, toDate, variant = 'explore' }) {
+    const wishlist = useSelector((state) => state.userModule.user?.wishlist) || []
     const days = differenceInDays(toDate, fromDate)
-    const [isFavorite, setIsFavorite] = useState(false)
+    const isFavorite = wishlist.includes(stay._id)
+    const setIsFavorite = () => {
+        if (isFavorite) removeFromWishlist(stay._id)
+        else addToWishlist(stay._id)
+    }
     const stayType = [
         'Apartment',
         'House',
@@ -37,19 +43,17 @@ export function StayPreview({ stay, fromDate, toDate, variant = 'explore' }) {
 
     return (
         // <Link to={`/stay/${stay._id}`} target="_blank" className="stay-preview">
-    //    testing 
+        //    testing
         <Link
-  to={`/stay/${stay._id}`}
-  className="stay-preview"
-  state={{
-    checkIn: fromDate,
-    checkOut: toDate,
-    guests: 1,
-    nights: differenceInDays(toDate, fromDate),
-    pricePerNight: stay.price
-  }}
->
-
+            to={`/stay/${stay._id}`}
+            className="stay-preview"
+            state={{
+                checkIn: fromDate,
+                checkOut: toDate,
+                guests: 1,
+                nights: differenceInDays(toDate, fromDate),
+                pricePerNight: stay.price,
+            }}>
             <div className="stay-inner-img">
                 <div className="img-overlay">
                     <div className="favorite-icon-container">
@@ -58,7 +62,7 @@ export function StayPreview({ stay, fromDate, toDate, variant = 'explore' }) {
                             onClick={(ev) => {
                                 ev.preventDefault()
                                 ev.stopPropagation()
-                                setIsFavorite((prev) => !prev)
+                                setIsFavorite()
                             }}
                             fill={isFavorite ? `var(--clr-brand)` : undefined}
                         />
@@ -73,8 +77,7 @@ export function StayPreview({ stay, fromDate, toDate, variant = 'explore' }) {
                 {variant === 'filtered' && (
                     <>
                         <h4 className="filtered-title">
-                            {randomStayType} in{' '}
-                            {stay.loc.city}
+                            {randomStayType} in {stay.loc.city}
                             <span className="filtered-rating">
                                 <StarIcon size={12} />
                                 &nbsp;{randomRating}&nbsp;({randomReviewCount})
@@ -98,8 +101,7 @@ export function StayPreview({ stay, fromDate, toDate, variant = 'explore' }) {
                 {variant === 'explore' && (
                     <>
                         <h4 className="explore-title">
-                            {randomStayType} in{' '}
-                            {stay.loc.city}
+                            {randomStayType} in {stay.loc.city}
                         </h4>
                         <div className="explore-details">
                             <span>
