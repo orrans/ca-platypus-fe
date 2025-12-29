54fd5bddacb1bff78746e2a472cfec65217fe506
Aviad Ben-Hamo <aviad.benhamo@gmail.com>
2025-12-29 10:28:58 +0200

    Refactor UserMsg component for improved readability and maintainability

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -3,40 +3,45 @@ import { useState, useEffect, useRef } from 'react'
 import { socketService, SOCKET_EVENT_REVIEW_ABOUT_YOU } from '../services/socket.service'
 
 export function UserMsg() {
-	const [msg, setMsg] = useState(null)
-	const timeoutIdRef = useRef()
+    const [msg, setMsg] = useState(null)
+    const timeoutIdRef = useRef()
 
-	useEffect(() => {
-		const unsubscribe = eventBus.on('show-msg', msg => {
-			setMsg(msg)
-			if (timeoutIdRef.current) {
-				timeoutIdRef.current = null
-				clearTimeout(timeoutIdRef.current)
-			}
-			timeoutIdRef.current = setTimeout(closeMsg, 3000)
-		})
+    useEffect(() => {
+        const unsubscribe = eventBus.on('show-msg', msg => {
+            setMsg(msg)
+            if (timeoutIdRef.current) {
+                timeoutIdRef.current = null
+                clearTimeout(timeoutIdRef.current)
+            }
+            timeoutIdRef.current = setTimeout(closeMsg, 3000)
+        })
 
-		socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
-			showSuccessMsg(`New review about me ${review.txt}`)
-		})
+        socketService.on(SOCKET_EVENT_REVIEW_ABOUT_YOU, review => {
+            showSuccessMsg(`New review about me ${review.txt}`)
+        })
 
-		return () => {
-			unsubscribe()
-			socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
-		}
-	}, [])
+        socketService.on('order-added', (order) => {
+            showSuccessMsg(`New Order! 🔔 ${order.guest.fullname} booked ${order.stay.name}`)
+        })
 
-	function closeMsg() {
-		setMsg(null)
-	}
+        return () => {
+            unsubscribe()
+            socketService.off(SOCKET_EVENT_REVIEW_ABOUT_YOU)
+            socketService.off('order-added')
+        }
+    }, [])
+
+    function closeMsg() {
+        setMsg(null)
+    }
 
     function msgClass() {
         return msg ? 'visible' : ''
     }
-	return (
-		<section className={`user-msg ${msg?.type} ${msgClass()}`}>
-			<button onClick={closeMsg}>x</button>
-			{msg?.txt}
-		</section>
-	)
+    return (
+        <section className={`user-msg ${msg?.type} ${msgClass()}`}>
+            <button onClick={closeMsg}>x</button>
+            {msg?.txt}
+        </section>
+    )
 }
